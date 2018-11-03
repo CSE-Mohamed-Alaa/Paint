dfeb30ac0eb0743bc8a0875b6b3bcb4b3f10317a
OWNER <OWNER@DESKTOP-3CK9NLN>
2018-11-04 01:01:08 +0200

    bonus done

--- Draw/src/eg/edu/alexu/csd/oop/draw/DrawingEngineImp.java
+++ Draw/src/eg/edu/alexu/csd/oop/draw/DrawingEngineImp.java
@@ -1,7 +1,14 @@
 package eg.edu.alexu.csd.oop.draw;
 
 import java.awt.Graphics;
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 public class DrawingEngineImp implements DrawingEngine {
 
@@ -37,7 +44,45 @@ public class DrawingEngineImp implements DrawingEngine {
 
 	@Override
 	public List<Class<? extends Shape>> getSupportedShapes() {
-		return null;
+		List<Class<? extends Shape>> ans = new ArrayList<Class<? extends Shape>>();
+		JarFile jarFile;
+		try {
+			jarFile = new JarFile("RoundRectangle.jar");
+		
+		Enumeration<JarEntry> e = jarFile.entries();
+
+		URL[] urls = { new URL("jar:file:" + "RoundRectangle.jar"+"!/") };
+		URLClassLoader cl = URLClassLoader.newInstance(urls);
+
+		while (e.hasMoreElements()) {
+		    JarEntry je = e.nextElement();
+		    if(je.isDirectory() || !je.getName().endsWith(".class")){
+		        continue;
+		    }
+		    // -6 because of .class
+		    String className = je.getName().substring(0,je.getName().length()-6);
+		    className = className.replace('/', '.');
+		    Class<? extends Shape> c = (Class<? extends Shape>) cl.loadClass(className);
+		    Object x = c.newInstance();
+		    if (x instanceof Shape) {
+				ans.add(c);
+			}
+
+		}
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return ans;
 	}
 
 	@Override
