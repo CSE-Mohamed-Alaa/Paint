e2e01a1eb7a366fac6ac447f4331f48dbd5922d9
MHMD_Alaa <mhmdalaa333@gmail.com>
2018-11-05 02:17:55 +0200

    GUI Started

---
+++ Draw/src/eg/edu/alexu/csd/oop/draw/GUI/DrawingBoard.java
@@ -0,0 +1,58 @@
+package eg.edu.alexu.csd.oop.draw.GUI;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JPanel;
+
+import eg.edu.alexu.csd.oop.draw.DrawingEngine;
+import eg.edu.alexu.csd.oop.draw.DrawingEngineImp;
+import eg.edu.alexu.csd.oop.draw.Line;
+import eg.edu.alexu.csd.oop.draw.Shape;
+
+public class DrawingBoard extends JPanel {
+	
+	DrawingEngine drawingEngine = new DrawingEngineImp();
+	
+	public void paintComponent(Graphics canvas) {
+		super.paintComponent(canvas);
+		this.setBackground(Color.WHITE);
+		/*
+		//line build in working 
+		canvas.setColor(Color.GREEN);
+		canvas.drawLine(0,0,50,50);
+		*/
+		
+		//it is the right implementation i think
+		Shape[] Shape = drawingEngine.getShapes();
+		for (Shape x : Shape) {
+			x.draw(canvas);
+		}
+		/*
+		//our line (Doesn't Work)
+		Line line = new Line();
+		line.setPoint1(new Point(0,0));
+		line.setPoint2(new Point(50,50));
+		line.setColor(Color.GREEN);
+		line.draw(canvas);
+		*/
+	}
+
+	public void addLine(Point start, Point end) {
+		//ta2rebn hanb3at al properties be Map 3a4an manb3at4 parameters kteer
+		//3a4an kda kda hatet7at fe al map , fa badal ma afdl a pass parameters kteer 
+		//a3ml pass le al Map we 5las
+		//wn3ml function wa7dah addShape bta5od al properties (Map) we 5las
+		//TODO delete this comments
+		Line line = new Line();
+		line.setPoint1(start);
+		line.setPoint2(end);
+		//TODO set colors we 7agat tanyah m4 wa2taha now
+		drawingEngine.addShape(line);
+		repaint();
+	}
+	
+	
+	
+}
---
+++ Draw/src/eg/edu/alexu/csd/oop/draw/GUI/GUI.java
@@ -0,0 +1,62 @@
+package eg.edu.alexu.csd.oop.draw.GUI;
+
+import javax.swing.JFrame;
+
+import java.awt.EventQueue;
+import java.awt.Graphics;
+import java.awt.Point;
+
+import javax.swing.JButton;
+
+public class GUI {
+	
+	/*
+	 * Catch Shape Properties :-
+	 * Catch name from combo or choice box with all shapes
+	 * Catch color and fill color from combo or choice box
+	 * Catch the properties with listeners or enter the properties in textField (Note: listeners Easier)
+	 * calculate the setPosition (different with every shape)
+	 * Call addShape
+	 */
+
+	private JFrame frame;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	public GUI() {
+		initialize();
+	}
+
+	private void initialize() {
+		frame = new JFrame("Paint");
+		frame.setResizable(false);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(600, 400);
+		frame.setVisible(true);
+		frame.getContentPane().setLayout(null);
+
+		DrawingBoard drawingBoard = new DrawingBoard();
+		drawingBoard.setBounds(10, 45, 574, 316);
+		frame.getContentPane().add(drawingBoard);
+
+		JButton drawLinebtn = new JButton("Draw Line");
+		drawLinebtn.addActionListener(e -> drawingBoard.addLine(null, null));
+		drawLinebtn.setBounds(10, 11, 89, 23);
+		frame.getContentPane().add(drawLinebtn);
+
+		JButton btnNewButton_1 = new JButton("New button");
+		btnNewButton_1.setBounds(109, 11, 89, 23);
+		frame.getContentPane().add(btnNewButton_1);
+	}
+}
